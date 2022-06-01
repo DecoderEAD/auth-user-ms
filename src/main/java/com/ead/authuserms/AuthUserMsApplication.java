9a76626b7b15fe1459183b856a2e257563dc5650
Paulo Viana <pauloc.fviana@hotmail.com>
2022-05-31 23:32:20 -0300

    initial commits

---
+++ src/main/java/com/ead/authuserms/AuthUserMsApplication.java
@@ -0,0 +1,13 @@
+package com.ead.authuserms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthUserMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthUserMsApplication.class, args);
+	}
+
+}
